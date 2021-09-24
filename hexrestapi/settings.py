b25fe0aa79fe637528dcf4fc3d7f2e0d9a00ae83
Krzysztof Kuta <atuk.krzysztof@gmail.com>
2021-09-24 22:49:48 +0200

    next commit

--- hexrestapi/settings.py
+++ hexrestapi/settings.py
@@ -16,8 +16,8 @@ from decouple import config
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
